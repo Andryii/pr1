e13fc5e0325226e8fa5ba2066eced69475c42786
Andryii <andryii.protasenko@gmial.com>
2022-08-08 16:01:31 +0300

    Learn React step 7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 
 function App() {
@@ -6,11 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
+      <ClassCounter/>
     </div>
   );
 }
