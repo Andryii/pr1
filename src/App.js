d0a93ddb6d90e7df280863c3fab9a7b5c9aa9993
Andryii <andryii.protasenko@gmial.com>
2022-08-10 23:40:27 +0300

    Error porocesing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { usePosts, useSortedPosts } from "./hooks/usePosts";
 
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -21,7 +22,11 @@ function App() {
   const [modal, setModal] = useState(false);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const sortedAndSerchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
+
   useEffect(() => {
     fetchPosts();
   }, []);
@@ -31,15 +36,6 @@ function App() {
     setModal(false);
   };
 
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts);
-      setIsPostsLoading(false);
-    }, 1000);
-  }
 
   function removePost(post) {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -57,8 +53,18 @@ function App() {
       <hr style={{ margin: "15px 0" }}></hr>
       <PostFilter filter={filter} setFilter={setFilter} />
 
+      {postError &&
+        <h1>Произошла ошибка ${postError}</h1>
+      }
+
       {isPostsLoading ? (
-        <div style={{ display: "flex", justifyContent: "center" , marginTop: "50px"}}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "50px",
+          }}
+        >
           <Loader />
         </div>
       ) : (
